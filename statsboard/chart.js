30427761b9f073aeb2401a246a0df9d6925ec78c
MeerkatPerson <t.tratzmueller@outlook.de>
2020-09-30 11:36:06 +0200

    Added profile data

--- statsboard/chart.js
+++ statsboard/chart.js
@@ -1,35 +1,50 @@
 window.addEventListener("DOMContentLoaded", draw);
 
-// Define data file as  a global variable
+// Define data files as  a global variable
 
-let dataFile;
+let rideData;
 
-// Define array which will contain data aggregated over all regions as  a global variable
+let profileData;
 
-let aggregatedData;
+// Define arrays which will contain data aggregated over all regions as  global variables
+
+let aggregatedRides;
+
+let aggregatedProfiles;
+
+// Grab the select-region menu from the DOM
 
 let selectMenu = document.getElementById("select-region");
 
 function draw() {
 
-  // Load the data file, compute the aggregation over all regions
+  // Load the data files, compute the aggregations over all regions
   // and draw charts using the aggregated data
 
-  let myPromise = fetch('./rideData.json')
-    .then(function(response) {
-      return response.json();
-    }).then(function(json) {
-        return json;
-    }).catch(function(error) {
-        console.error(error);
-    });
+  // (a) RIDES
 
-  myPromise.then((json) => {
-    dataFile = json;
-    aggregatedData = computeAggregation(json);
+  async function loadData() {
+
+    const rides = fetch('./rideData.json').then((response) => response.json());
+    const profiles = fetch('./profileData.json').then((response) => response.json());
+
+    const res = await Promise.all([rides, profiles]);
+
+    return res
+
+  }
+
+  loadData().then((dataArr) => {
+
+    rideData = dataArr[0];
+    profileData = dataArr[1];
+    aggregatedRides= computeAggregation(rideData);
+    aggregatedProfiles= computeAggregation(profileData);
+
+  // (b) PROFILES
 
     // Populate the options menu with available regions
-    Object.keys(dataFile).forEach((key) => {
+    Object.keys(rideData).forEach((key) => {
     var opt = document.createElement("option");
     opt.value = key;
     opt.innerHTML = key;
@@ -41,9 +56,13 @@ function draw() {
 
     selectMenu.addEventListener("change", selectOpt);
 
-    drawChart(aggregatedData.map(e => e.Files), aggregatedData.map(e => e.Date));
+    drawChart(aggregatedRides.map(e => e.Files), aggregatedRides.map(e => e.Date), 'Rides');
+
+    drawChart(aggregatedProfiles.map(e => e.Files), aggregatedProfiles.map(e => e.Date), 'Profiles');
+
 
   })
+
 
 }
 
@@ -73,23 +92,27 @@ function selectOpt() {
 
   if (selection == "Region auswählen") {
 
-    drawChart(aggregatedData.map(e => e.Files), aggregatedData.map(e => e.Date));
+    drawChart(aggregatedRides.map(e => e.Files), aggregatedRides.map(e => e.Date), 'Rides');
+    drawChart(aggregatedRides.map(e => e.Files), aggregatedRides.map(e => e.Date), 'Profiles');
 
   }
 
   else {
 
-    let regionData = dataFile[selection];
+    let regionRides = rideData[selection];
+    let regionProfiles = profileData[selection];
 
-    regionData.shift();
+    regionRides.shift();
+    regionProfiles.shift();
 
-    drawChart(regionData.map(e => e.Files), regionData.map(e => e.Date));
+    drawChart(regionRides.map(e => e.Files), regionRides.map(e => e.Date), 'Rides');
+    drawChart(regionRides.map(e => e.Files), regionRides.map(e => e.Date), 'Profiles');
 
   }
 
 }
 
-function drawChart(graphData, graphLabelsRaw) {
+function drawChart(graphData, graphLabelsRaw, dataCat) {
 
   // Some global options
   Chart.defaults.global.defaultFontFamily = 'Raleway';
@@ -124,7 +147,7 @@ function drawChart(graphData, graphLabelsRaw) {
 
   }
 
-  var dailyHist = document.getElementById('dailyHist').getContext('2d');
+  var dailyHist = document.getElementById(`dailyHist_${dataCat}`).getContext('2d');
 
   var dailyUploadsChart = new Chart(dailyHist, {
     type: 'bar', // bar, horizontal, pie, line, doughnut, radar, polarArea
@@ -148,7 +171,7 @@ function drawChart(graphData, graphLabelsRaw) {
       title: {
         display: true,
         fontFamily: 'Poiret One',
-        text: 'Uploads/Tag der letzten sieben Tage',
+        text: `${dataCat}/Tag der letzten sieben Tage`,
         fontSize: 25,
         fontColor: 'grey'
       },
@@ -173,428 +196,93 @@ function drawChart(graphData, graphLabelsRaw) {
     }
   });
 
-  /*2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2
-  /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // LINE CHART DISPLAYING CUMULATIVE DAILY UPLOADS */
-
-  var cumulativeDailyData = graphData.reduce(function(r, a) {
-    if (r.length > 0)
-      a += r[r.length - 1];
-    r.push(a);
-    return r;
-  }, []);
-
-  var dailyCumulativeChart = document.getElementById('dailyCumulativeChart').getContext('2d');
-
-  var dailyCumulativeUploadsChart = new Chart(dailyCumulativeChart, {
-    type: 'line', // bar, horizontal, pie, line, doughnut, radar, polarArea
-    data: {
-      labels: graphLabelsString,
-      datasets: [{
-        label: 'Cumulative uploads',
-        data: cumulativeDailyData,
-        backgroundColor: 'lightsalmon',
-        //backgroundColor: []
-        borderWidth: 1,
-        borderColor: 'salmon',
-        hoverBorderWidth: 2,
-        hoverBorderBackground: 'salmon',
-        hoverBorderColor: 'tomato',
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Uploads/Tag seit Projektbeginn, kumuliert',
-        fontSize: 25,
-        fontFamily: 'Poiret One',
-        fontColor: 'grey'
-      },
-      legend: {
-        display: false,
-        position: 'right',
-        labels: {
-          fontColor: 'black'
-        }
-      },
-      layout: {
-        padding: {
-          left: 50,
-          right: 0,
-          bottom: 0,
-          top: 0
-        }
-      },
-      tooltips: {
-        enabled: true
-      },
-      scales: {
-        xAxes: [{
-          ticks: {
-            fontSize: 14,
-            autoskip: true,
-            autoSkipPadding: 50
-          },
-          display: true
-        }]
-      }
-    }
-  });
-
-  /* 3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3-3
-  /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-  // HISTOGRAM DISPLAYING WEEKLY UPLOADS
-  // ********************************************************************
-  // Transform daily data to weekly data.
-  // --> need to be aware of the fact that the first and last week may
-  //     be shorter as the first data point was probably not sent on a
-  //     Monday and the last day data has been received was probably
-  //     not a Sunday.
-
-  // Array in which # of uploads per week will be stored
-  var weeklyData = [];
-
-  // Array in which labels for weeks will be stored, e.g.
-  // 'Mon, 27th of May, 2019 - Sun, 2nd of June, 2019'
-  var weeklyLabels = [];
-
-  // Helper variable for accumulating weekly upload data
-  var accumulator = 0;
-
-  // Helper variable for storing beginnings of weeks
-  var startOfWeek_temp = "";
-
-  // Helper variable for storing ends of weeks
-  var endOfWeek_temp = "";
-
-  // Here comes the loop, looping over our daily data and condensing it into
-  // weekly data
-  for (i = 0; i < graphData.length; i++) {
-
-    console.log(graphLabels[i].isoWeekday());
-
-    switch (graphLabels[i].isoWeekday()) {
-
-      case 1: // Monday
-
-        accumulator += graphData[i];
-        startOfWeek_temp = graphLabels[i].format('DD-MM-YY');
-
-        break;
-
-      case 7: // Sunday
-
-        accumulator += graphData[i];
-        endOfWeek_temp = graphLabels[i].format('DD-MM-YY');
-
-        weeklyData.push(accumulator);
-        accumulator = 0;
-
-        weeklyLabels.push(startOfWeek_temp.concat(' - ', endOfWeek_temp));
-
-        startOfWeek_temp = "";
-        endOfWeek_temp = "";
-
-        break;
-
-      default: // All other weekdays
-
-        accumulator += graphData[i];
-
-        // Here we're taking care of the case in which the first data datapoint
-        // in the data set wasn't a Monday
-        if (i == 0) {
-
-          startOfWeek_temp = graphLabels[i].format('DD-MM-YY');
-
-        }
-
-        // Here we're taking care of the case in which the last data datapoint
-        // in the data set wasn't a Sunday
-
-        if (i == (graphData.length - 1)) {
-
-          endOfWeek_temp = graphLabels[i].format('DD-MM-YY');
-
-          weeklyData.push(accumulator);
-          accumulator = 0; // superfluous, but neat
-
-          weeklyLabels.push(startOfWeek_temp.concat(' - ', endOfWeek_temp));
-
-          startOfWeek_temp = ""; // superfluous, but neat
-          endOfWeek_temp = ""; // superfluous, but neat
-
-          break;
-
-        }
-
-    }
-
-  }
-
-  var weeklyHist = document.getElementById('weeklyHist').getContext('2d');
-
-  var weeklyUploadsChart = new Chart(weeklyHist, {
-    type: 'bar', // bar, horizontal, pie, line, doughnut, radar, polarArea
-    data: {
-      //labels:graphLabels.slice(-7),
-      labels: weeklyLabels,
-      datasets: [{
-        label: 'Uploads',
-        data: weeklyData,
-        //data:graphData.slice(-7),
-        backgroundColor: 'lightsalmon',
-        //backgroundColor: []
-        borderWidth: 1,
-        borderColor: 'salmon',
-        hoverBorderWidth: 2,
-        hoverBorderBackground: 'salmon',
-        hoverBorderColor: 'tomato',
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        fontFamily: 'Poiret One',
-        text: 'Uploads/Woche seit Projektbeginn',
-        fontSize: 25,
-        fontColor: 'grey'
-      },
-      legend: {
-        display: false,
-        position: 'right',
-        labels: {
-          fontColor: 'black'
-        }
-      },
-      layout: {
-        padding: {
-          left: 50,
-          right: 0,
-          bottom: 0,
-          top: 0
-        }
-      },
-      tooltips: {
-        enabled: true
-      }
-    }
-  });
-
-  /* 4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4-4
-  /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // LINE CHART DISPLAYING CUMULATIVE WEEKLY UPLOADS */
-  /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-  var cumulativeWeeklyData = weeklyData.reduce(function(r, a) {
-    if (r.length > 0)
-      a += r[r.length - 1];
-    r.push(a);
-    return r;
-  }, []);
-
-  var weeklyCumulativeChart = document.getElementById('weeklyCumulativeChart').getContext('2d');
-
-  var weeklyCumulativeUploadsChart = new Chart(weeklyCumulativeChart, {
-    type: 'line', // bar, horizontal, pie, line, doughnut, radar, polarArea
-    data: {
-      labels: weeklyLabels,
-      datasets: [{
-        label: 'Uploads',
-        data: cumulativeWeeklyData,
-        backgroundColor: 'lightsalmon',
-        //backgroundColor: []
-        borderWidth: 1,
-        borderColor: 'salmon',
-        hoverBorderWidth: 2,
-        hoverBorderBackground: 'salmon',
-        hoverBorderColor: 'tomato',
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Uploads/Woche seit Projektbeginn, kumuliert',
-        fontSize: 25,
-        fontFamily: 'Poiret One',
-        fontColor: 'grey'
-      },
-      legend: {
-        display: false,
-        position: 'right',
-        labels: {
-          fontColor: 'black'
-        }
-      },
-      layout: {
-        padding: {
-          left: 50,
-          right: 0,
-          bottom: 0,
-          top: 0
-        }
-      },
-      tooltips: {
-        enabled: true
-      },
-      scales: {
-        xAxes: [{
-          ticks: {
-            fontSize: 14,
-            userCallback: function(item, index) {
-              if (index % 2) return "";
-              return item;
-            },
-            autoSkip: false
-          },
-          display: true
-        }]
-      }
-    }
-  });
-
-  /* 5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5-5
-  /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-  // HISTOGRAM DISPLAYING MONTHLY UPLOADS
-  // ********************************************************************
-  // Transform daily data to monthly data.
-  // --> need to be aware of (...)
-
-  // Array in which # of uploads per month will be stored
-  var monthlyData = [];
-
-  // Array in which labels for months will be stored, e.g.
-  // 'April 2019'
-  var monthlyLabels = [];
-
-  // Helper variable for accumulating monthly upload data
-  var accumulator = 0;
-
-  // Here comes the loop, looping over our daily data and condensing it into
-  // monthly data
-  for (i = 0; i < graphData.length; i++) {
-
-    //console.log(moment(graphLabels[i], 'ddd, MMMM Do YYYY').isoWeekday());
-
-    // As the function is based on comparing the current data point's month with
-    // the previous data points', we firstly have to take care of the case in which
-    // the current data point is the first one in the data
-
-    switch (i) {
-
-      case 0:
-
-        accumulator += graphData[i];
-
-        break;
-
-      case (graphData.length - 1):
-
-        monthlyLabels.push(graphLabels[i].format('MMMM').concat(' ',
-          graphLabels[i].format('YYYY')));
-
-        monthlyData.push(accumulator);
-        accumulator = 0;
-
-        break;
-
-      default:
-
-        if (graphLabels[i].month() ==
-          graphLabels[i - 1].month()) {
-
-          accumulator += graphData[i];
-
-        } else {
-
-          monthlyLabels.push(graphLabels[i - 1].format('MMMM').concat(' ',
-            graphLabels[i - 1].format('YYYY')));
-
-          monthlyData.push(accumulator);
-          accumulator = 0;
-
-        }
-
-        break;
-
-    }
-
-  }
-
-  // Create JSONarray
-
-  var obj = {
-    jsonarray: []
-  };
-
-  // Combine the data and label arrays into on JSON file
-
-  for (i = 0; i < monthlyData.length; i++) {
-
-    var month_year = monthlyLabels[i];
-
-    var files = monthlyData[i];
-
-    obj.jsonarray.push({
-      Date: month_year,
-      Files: files
-    });
-
-  }
-
-  var monthlyHist = document.getElementById('monthlyHist').getContext('2d');
-
-  var monthlyUploadsChart = new Chart(monthlyHist, {
-    type: 'bar', // bar, horizontal, pie, line, doughnut, radar, polarArea
-    data: {
-      //labels:graphLabels.slice(-7),
-      labels: monthlyLabels,
-      datasets: [{
-        label: 'Uploads',
-        data: monthlyData,
-        //data:graphData.slice(-7),
-        backgroundColor: 'lightsalmon',
-        //backgroundColor: []
-        borderWidth: 1,
-        borderColor: 'salmon',
-        hoverBorderWidth: 2,
-        hoverBorderBackground: 'salmon',
-        hoverBorderColor: 'tomato',
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        fontFamily: 'Poiret One',
-        text: 'Uploads/Monat seit Projektbeginn',
-        fontSize: 25,
-        fontColor: 'grey'
-      },
-      legend: {
-        display: false,
-        position: 'right',
-        labels: {
-          fontColor: 'black'
-        }
-      },
-      layout: {
-        padding: {
-          left: 50,
-          right: 0,
-          bottom: 0,
-          top: 0
-        }
-      },
-      tooltips: {
-        enabled: true
-      }
-    }
-  });
-
-  /* 6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6-6
+  /* 2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2-2
   /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // LINE CHART DISPLAYING CUMULATIVE MONTHLY UPLOADS */
   /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+    // Array in which # of uploads per month will be stored
+    var monthlyData = [];
+
+    // Array in which labels for months will be stored, e.g.
+    // 'April 2019'
+    var monthlyLabels = [];
+  
+    // Helper variable for accumulating monthly upload data
+    var accumulator = 0;
+  
+    // Here comes the loop, looping over our daily data and condensing it into
+    // monthly data
+    for (i = 0; i < graphData.length; i++) {
+  
+      //console.log(moment(graphLabels[i], 'ddd, MMMM Do YYYY').isoWeekday());
+  
+      // As the function is based on comparing the current data point's month with
+      // the previous data points', we firstly have to take care of the case in which
+      // the current data point is the first one in the data
+  
+      switch (i) {
+  
+        case 0:
+  
+          accumulator += graphData[i];
+  
+          break;
+  
+        case (graphData.length - 1):
+  
+          monthlyLabels.push(graphLabels[i].format('MMMM').concat(' ',
+            graphLabels[i].format('YYYY')));
+  
+          monthlyData.push(accumulator);
+          accumulator = 0;
+  
+          break;
+  
+        default:
+  
+          if (graphLabels[i].month() ==
+            graphLabels[i - 1].month()) {
+  
+            accumulator += graphData[i];
+  
+          } else {
+  
+            monthlyLabels.push(graphLabels[i - 1].format('MMMM').concat(' ',
+              graphLabels[i - 1].format('YYYY')));
+  
+            monthlyData.push(accumulator);
+            accumulator = 0;
+  
+          }
+  
+          break;
+  
+      }
+  
+    }
+  
+    // Create JSONarray
+  
+    var obj = {
+      jsonarray: []
+    };
+  
+    // Combine the data and label arrays into on JSON file
+  
+    for (i = 0; i < monthlyData.length; i++) {
+  
+      var month_year = monthlyLabels[i];
+  
+      var files = monthlyData[i];
+  
+      obj.jsonarray.push({
+        Date: month_year,
+        Files: files
+      });
+  
+    }
+  
 
   var cumulativeMonthlyData = monthlyData.reduce(function(r, a) {
     if (r.length > 0)
@@ -603,7 +291,7 @@ function drawChart(graphData, graphLabelsRaw) {
     return r;
   }, []);
 
-  var monthlyCumulativeChart = document.getElementById('monthlyCumulativeChart').getContext('2d');
+  var monthlyCumulativeChart = document.getElementById(`monthlyCumulative_${dataCat}`).getContext('2d');
 
   var monthlyCumulativeUploadsChart = new Chart(monthlyCumulativeChart, {
     type: 'line', // bar, horizontal, pie, line, doughnut, radar, polarArea
@@ -624,7 +312,7 @@ function drawChart(graphData, graphLabelsRaw) {
     options: {
       title: {
         display: true,
-        text: 'Uploads/Monat seit Projektbeginn, kumuliert',
+        text: `${dataCat}/Monat seit Projektbeginn, kumuliert`,
         fontSize: 25,
         fontFamily: 'Poiret One',
         fontColor: 'grey'
